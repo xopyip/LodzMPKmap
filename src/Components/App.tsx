034c6e3c7bfbf3ac8afcc7519d2fbad64f6bc06f
Mateusz Bałuch <xopyip@users.noreply.github.com>
2022-04-23 21:20:01 +0200

    fix type of tileLayerRef

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -21,18 +21,18 @@ function App() {
   const [isTopographic, setIsTopographic] = useState(true);
 
   const alert = useAlert();
-  const tileLayerRef = useRef<LeafletTileLayer>(null);
+  const tileLayerRef = useRef<LeafletTileLayer | null>(null);
   const topographicURL =
     "https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png";
   const topographicAttribution =
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>';
+    "&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors, Tiles style by <a href=\"https://www.hotosm.org/\" target=\"_blank\">Humanitarian OpenStreetMap Team</a> hosted by <a href=\"https://openstreetmap.fr/\" target=\"_blank\">OpenStreetMap France</a>";
   const nonTopographicURL =
     "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}";
   const nonTopographicAttribution =
     "Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012";
   useEffect(() => {
     if (tileLayerRef.current) {
-      tileLayerRef.current.setUrl(
+      tileLayerRef.current!.setUrl(
         isTopographic ? topographicURL : nonTopographicURL,
       );
     }
